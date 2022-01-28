0555eb1845ed164b601b7e08e32e01fa1bb3d3e7
Ethan Ennis <ethan_ennis_8@icloud.com>
2022-01-28 13:36:38 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Ethan Ennis on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
